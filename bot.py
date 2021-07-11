fd0bf0480cb5a5c4cbe60580b62e7d2285f1f0ca
alsdream <AlsdnWorks@gmail.com>
2021-07-12 03:26:01 +0900

    first

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+from selenium import webdriver 
+import time
+client = commands.Bot(command_prefix='*')
+@client.command()
+async def 조회(look):    
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+    options.add_argument("disable-gpu")
+    browser = webdriver.Chrome('chromedriver.exe',options=options)
+    url = 'https://aircis.kr/jspView.do?jsp=kor/schedule/airDailySchLi'
+    browser.get(url)
+    #time.sleep(3) 이거 네트워크 상테 불량할때 켜주세요
+    data = browser.find_element_by_css_selector("#gridMasterLayer > table").text
+    print(data)
+    await look.send(data)
+    browser.quit()
+
+
+
+
+    
+client.run('ODYzODI0OTk3MDIxNzc3OTcx.YOshuA.1L8hGK8yaHvBq6Tj9rxcRJmSpKI')
