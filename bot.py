d8fb21b46500b5087b657db144504eb01751e339
alsdream <AlsdnWorks@gmail.com>
2021-07-12 14:15:08 +0900

    해치웠나??

--- bot.py
+++ bot.py
@@ -5,12 +5,15 @@ from selenium import webdriver
 import time
 client = commands.Bot(command_prefix='*')
 @client.command()
-async def 조회(look):    
+async def 조회(look):   #' *조회'    
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     options.add_argument('window-size=1920x1080')
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
     options.add_argument("disable-gpu")
-    browser = webdriver.Chrome('chromedriver.exe',options=options)
+    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")#heroku 외부변수
+    browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)#heroku 외부변수
     url = 'https://aircis.kr/jspView.do?jsp=kor/schedule/airDailySchLi'
     browser.get(url)
     #time.sleep(3) 이거 네트워크 상테 불량할때 켜주세요
@@ -18,8 +21,4 @@ async def 조회(look):
     print(data)
     await look.send(data)
     browser.quit()
-
-
-
-    
-client.run('os.environ['token'])
+client.run('os.environ['token'])#heroku에서 자동으로 토큰삽입합니다
